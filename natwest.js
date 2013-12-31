0cb5bf32dcfe1646182342fe3c1f373143119378
Matt Harrison <mt.harrison86@gmail.com>
2013-12-31 17:16:04 +0000

    Added grunt and updated to prefer global

--- natwest.js
+++ natwest.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
+
 var credentials = {},
-phantom = require('node-phantom'),
-prompt = require("prompt"),
-fs = require("fs");
+    phantom = require('node-phantom'),
+    prompt = require("prompt"),
+    fs = require("fs");
 
 var argv = require('optimist').argv;
 
-if(typeof(argv.r) !== 'undefined'){
-    if(argv.r === "server"){
+if (typeof (argv.r) !== 'undefined') {
+    if (argv.r === "server") {
         var renderer = require("./renderers/ServerRenderer");
     } else {
         var renderer = require("./renderers/CLITableRenderer");
@@ -32,7 +33,7 @@ fs.exists('./credentials.json', function (exists) {
         console.log("");
         prompt.get({
             properties: {
-                'customer-number': {
+                'customer_number': {
                     description: 'Customer number: '.red.bold,
                     type: 'string',
                     required: true
@@ -64,9 +65,9 @@ fs.exists('./credentials.json', function (exists) {
 function connect(err, result) {
 
     if (result) {
-        credentials['customer-number'] = result['customer-number'];
-        credentials['pin'] = result['pin'];
-        credentials['password'] = result['password'];
+        credentials.customer_number = result.customer_number;
+        credentials.pin = result.pin;
+        credentials.password = result.password;
 
         if (result.write === 'y')
             fs.writeFile("credentials.json", JSON.stringify(credentials, null, 4));
@@ -76,37 +77,37 @@ function connect(err, result) {
 }
 
 function bank() {
-    console.log("Connecting to bank…")
+    console.log("Connecting to bank…");
     phantom.create(function (err, ph) {
         return ph.createPage(function (err, page) {
             return page.open("https://www.nwolb.com/", function (err, status) {
                 console.log("Entering customer number…");
                 setTimeout(function () {
                     return page.evaluate(function (credentials) {
-                        window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_LI5TABA_DBID_edit').value = credentials['customer-number'];
-                        window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_LI5TABA_LI5-LBA_button_button').click();
+                        window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_LI5TABA_DBID_edit').value = credentials.customer_number;
+                        window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_LI5TABA_LI5-LBA_button_button').click();
                     }, function (error, result) {
                         console.log("Entering pin and password…");
                         setTimeout(function () {
                             return page.evaluate(function (credentials) {
 
-                                var pin1 = parseInt(window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6DDALALabel').innerText.split(" ")[2].split("nd")[0]);
-                                var pin2 = parseInt(window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6DDALBLabel').innerText.split(" ")[2].split("nd")[0]);
-                                var pin3 = parseInt(window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6DDALCLabel').innerText.split(" ")[2].split("nd")[0]);
+                                var pin1 = parseInt(window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6DDALALabel').innerText.split(" ")[2].split("nd")[0]);
+                                var pin2 = parseInt(window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6DDALBLabel').innerText.split(" ")[2].split("nd")[0]);
+                                var pin3 = parseInt(window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6DDALCLabel').innerText.split(" ")[2].split("nd")[0]);
 
-                                window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6PPEA_edit').value = credentials.pin[pin1 - 1];
-                                window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6PPEB_edit').value = credentials.pin[pin2 - 1];
-                                window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6PPEC_edit').value = credentials.pin[pin3 - 1];
+                                window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6PPEA_edit').value = credentials.pin[pin1 - 1];
+                                window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6PPEB_edit').value = credentials.pin[pin2 - 1];
+                                window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6PPEC_edit').value = credentials.pin[pin3 - 1];
 
-                                var password1 = parseInt(window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6DDALDLabel').innerText.split(" ")[2].split("nd")[0]);
-                                var password2 = parseInt(window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6DDALELabel').innerText.split(" ")[2].split("nd")[0]);
-                                var password3 = parseInt(window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6DDALFLabel').innerText.split(" ")[2].split("nd")[0]);
+                                var password1 = parseInt(window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6DDALDLabel').innerText.split(" ")[2].split("nd")[0]);
+                                var password2 = parseInt(window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6DDALELabel').innerText.split(" ")[2].split("nd")[0]);
+                                var password3 = parseInt(window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6DDALFLabel').innerText.split(" ")[2].split("nd")[0]);
 
-                                window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6PPED_edit').value = credentials.password[password1 - 1];
-                                window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6PPEE_edit').value = credentials.password[password2 - 1];
-                                window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_LI6PPEF_edit').value = credentials.password[password3 - 1];
+                                window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6PPED_edit').value = credentials.password[password1 - 1];
+                                window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6PPEE_edit').value = credentials.password[password2 - 1];
+                                window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_LI6PPEF_edit').value = credentials.password[password3 - 1];
 
-                                window.frames['ctl00_secframe'].document.querySelector('#ctl00_mainContent_Tab1_next_text_button_button').click();
+                                window.frames.ctl00_secframe.document.querySelector('#ctl00_mainContent_Tab1_next_text_button_button').click();
 
                             }, function (error, result) {
                                 setTimeout(function () {
@@ -115,15 +116,15 @@ function bank() {
                                         var accounts = [];
 
 
-                                        var accountsDOM = Array.prototype.slice.call(frames['ctl00_secframe'].document.querySelectorAll("#ctl00_mainContent_Accounts_Accounts .CollapseExpandLink"),0);
-                                        for(var i in accountsDOM){
+                                        var accountsDOM = Array.prototype.slice.call(frames.ctl00_secframe.document.querySelectorAll("#ctl00_mainContent_Accounts_Accounts .CollapseExpandLink"), 0);
+                                        for (var i in accountsDOM) {
                                             var account = {};
-                                            account['id'] = parseInt(i)+1;
-                                            account['name'] = accountsDOM[i].querySelector(".AccountName").innerText;
-                                            account['number'] = accountsDOM[i].querySelector(".AccountNumber .num").innerText.replace(/ /g, "");
-                                            account['sortcode'] = accountsDOM[i].querySelector(".SortCode .num").innerText.replace(/ /g, "");
-                                            account['balance'] = accountsDOM[i].querySelectorAll(".currency")[0].innerText.replace(/ /g, "");
-                                            account['available'] = accountsDOM[i].querySelectorAll(".currency")[1].innerText.replace(/ /g, "");
+                                            account.id = parseInt(i) + 1;
+                                            account.name = accountsDOM[i].querySelector(".AccountName").innerText;
+                                            account.number = accountsDOM[i].querySelector(".AccountNumber .num").innerText.replace(/ /g, "");
+                                            account.sortcode = accountsDOM[i].querySelector(".SortCode .num").innerText.replace(/ /g, "");
+                                            account.balance = accountsDOM[i].querySelectorAll(".currency")[0].innerText.replace(/ /g, "");
+                                            account.available = accountsDOM[i].querySelectorAll(".currency")[1].innerText.replace(/ /g, "");
                                             accounts.push(account);
                                         }
 
@@ -132,13 +133,13 @@ function bank() {
                                         renderer.setData(result).render();
                                         ph.exit();
                                     }, credentials);
-}, 3000);
-}, credentials);
-}, 3000);
-}, credentials);
-}, 3000);
+                                }, 3000);
+                            }, credentials);
+                        }, 3000);
+                    }, credentials);
+                }, 3000);
 
-});
-});
-});
+            });
+        });
+    });
 }
